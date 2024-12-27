41b4d86098a4b9b61e2a070e17c31f868dc22315
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-27 12:29:02 +0100

    javascript

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,18 @@ const sr=ScrollReveal({
 })
 sr.reveal(".first-section, .second-section, .third-section, .inscription, .fourth-section, .copy", {interval:200})
 
+
+// partie du code du menu burger
+
+const burger=document.querySelector(".bars");
+const rem=document.querySelector(".remov");
+const navigation=document.querySelector("header ul");
+
+burger.addEventListener("click", ()=>{
+    navigation.classList.toggle("active")
+   
+})
+window.addEventListener("scroll", ()=>{
+    navigation.classList.remove("active")
+    burger.style.display='block'
+})
