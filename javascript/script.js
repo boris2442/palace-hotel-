4eaae98eb95e145b03e2da5f7f23139575678271
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-27 11:32:25 +0100

    inclusion de la librairie scrollReveal

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+// partie destinee au scrollReveal
+
+const sr=ScrollReveal({
+    duration:1500,
+    origin:"top",
+    distance:"30px",
+    reset:true
+})
+sr.reveal(".first-section, .second-section, .third-section, .inscription, .fourth-section, .copy", {interval:200})
+
