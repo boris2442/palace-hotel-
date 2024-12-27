ee267337f53e521aab697d625d90096b43e3550d
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-27 12:50:17 +0100

    text

--- javascript/script.js
+++ javascript/script.js
@@ -23,3 +23,31 @@ window.addEventListener("scroll", ()=>{
     navigation.classList.remove("active")
     burger.style.display='block'
 })
+
+// partie destinee a la librairie typewriter
+
+const child=document.querySelector(".child-title")
+const typewriter= new Typewriter(child, {
+    loop:true,
+    delay:150
+})
+.typeString("Un lieu unique pour un séjour unique")
+.pauseFor(2000)
+.deleteAll()
+.pauseFor(2000)
+.start()
+
+
+
+
+// decouverte
+const decouvert=document.querySelector(".decouverte")
+const app=new Typewriter(decouvert, {
+    loop:true,
+    delay:150
+})
+.typeString("et à decouvrir")
+.pauseFor(2000)
+.deleteAll()
+.pauseFor(2000)
+.start()
